b00621a57edd5ef68039b1a37903ad425deb1fe8
wesley601 <wesleypassos601@gmail.com>
2021-01-31 12:14:21 -0300

    Add some Twiggy goodness

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function homepage()
@@ -16,9 +17,15 @@ class QuestionController
     #[Route('/questions/{slug}', name: 'show')]
     public function show(string $slug)
     {
-        return new Response(sprintf(
-            'Future page to show the question "%s"!',
-            $slug
-        ));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
